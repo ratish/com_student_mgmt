91caf2861f187eceb838e9e47abcab477c935911
Ratish Shrestha <ratish@selu.edu>
2020-12-07 17:38:31 -0600

    Change dashboard card title color to default

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 <div class="card-deck">
     <div class="card">
         <div class="card-body">
-            <h3 class="card-title border-bottom text-primary"><i class="fas fa-user fa-lg"></i> Student Management</h3>
+            <h3 class="card-title border-bottom"><i class="fas fa-user fa-lg"></i> Student Management</h3>
             <p class="card-text">View students, add new student and edit student information</p>
             <div class="text-right">
                 <a href="{{ url('students') }}" class="btn btn-primary">Open Student Management <i
@@ -14,7 +14,7 @@
     </div>
     <div class="card">
         <div class="card-body">
-            <h3 class="card-title border-bottom text-primary"><i class="fas fa-book fa-lg"></i> Course Management</h3>
+            <h3 class="card-title border-bottom"><i class="fas fa-book fa-lg"></i> Course Management</h3>
             <p class="card-text">View course, add new course and edit course information</p>
             <div class="text-right">
                 <a href="{{ url('courses') }}" class="btn btn-primary">Open Course Management <i
